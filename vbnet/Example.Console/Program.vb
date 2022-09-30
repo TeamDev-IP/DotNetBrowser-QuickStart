f27ad295059f6c650e105f194e1a1dacabb1b242
Anna Dolbina <1564418+anna-dolbina@users.noreply.github.com>
2022-09-30 16:58:33 +0300

    Add space in the comment

--- vbnet/Example.Console/Program.vb
+++ vbnet/Example.Console/Program.vb
@@ -30,7 +30,7 @@ Namespace Example.Console
 		Public Shared Sub Main(args() As String)
 		    Dim builder = new EngineOptions.Builder()
 		    ' Uncomment the line below to specify your license key
-		    'builder.LicenseKey = "your_license_key"
+		    ' builder.LicenseKey = "your_license_key"
 
 			Using engine As IEngine = EngineFactory.Create(builder.Build())
 				Dim browser As IBrowser = engine.CreateBrowser()
