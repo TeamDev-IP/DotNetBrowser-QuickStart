5ee187b6c6e4fd59631ad636281c0cb8445c707e
Vladyslav Lubenskyi <vladyslav.lubenskyi@teamdev.com>
2023-08-15 10:34:10 +0200

    Load documentation portal vs HTML5 test

--- vbnet/Embedding.Wpf/MainWindow.xaml.vb
+++ vbnet/Embedding.Wpf/MainWindow.xaml.vb
@@ -33,7 +33,7 @@ Namespace Embedding.Wpf
     Partial Public Class MainWindow
         Inherits Window
 
-        Private Const Url As String = "https://html5test.com/"
+        Private Const Url As String = "https://dotnetbrowser-support.teamdev.com/"
         Private ReadOnly browser As IBrowser
         Private ReadOnly engine As IEngine
 
