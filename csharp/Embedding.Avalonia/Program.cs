afe46886c8bbe7cff0300f7c5237927f7663398e
Vladislav Tsvek <vladislav.tsvek@teamdev.com>
2023-10-03 12:30:43 +0300

    Fix compile issue and warnings

--- csharp/Embedding.Avalonia/Program.cs
+++ csharp/Embedding.Avalonia/Program.cs
@@ -37,8 +37,7 @@ namespace Embedding.AvaloniaUi
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .WithInterFont()
-                .LogToTrace();
+                         .UsePlatformDetect()
+                         .LogToTrace();
     }
 }
